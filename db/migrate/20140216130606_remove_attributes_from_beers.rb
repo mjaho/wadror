5b46c9e5fe363efb3ad905a2befa0ca97d4a760c
Mika Aho <mjaho@cs.helsinki.fi>
2014-02-16 17:27:44 +0200

    Bugfix

--- db/migrate/20140216130606_remove_attributes_from_beers.rb
+++
@@ -1,6 +0,0 @@
-class RemoveAttributesFromBeers < ActiveRecord::Migration
-  def change
-    remove_column :beers, :beer_id_id, :integer
-    remove_column :beers, :beer_id_type, :string
-  end
-end
--- db/migrate/20140216131616_remove_styles_id_from_beers.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStylesIdFromBeers < ActiveRecord::Migration
-  def change
-      remove_column :beers, :styles_id, :integer
-  end
-end
